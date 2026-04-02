74a0be42fb71e33f8f0a6bf9d6b2bae6f199b158
sadiqqenberov <sadiqqenberov26@gmail.com>
2026-04-02 17:01:34 +0400

    Eli 1

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,9 @@ public class Main {
     }
 
     public static void myMethodIsmayil3(){
-        System.out.println("myMethodIsmayil3");
+        System.out.println("myMethodIsmayil323");
+    }
+    public static void myMethodEli4(){
+        System.out.println("myMethodEli4");
     }
 }
